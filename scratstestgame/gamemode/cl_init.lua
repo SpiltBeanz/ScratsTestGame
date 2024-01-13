ec2bf407e8b82a0004515c0db4ba85acc556137b
SpiltBeanz <100332300+SpiltBeanz@users.noreply.github.com>
2024-01-13 03:40:28 -0500

    Delete scratstestgame directory
    broken

--- scratstestgame/gamemode/cl_init.lua
+++
@@ -1,6 +0,0 @@
-include("shared.lua")
-
--- Include Other Files
-
-include("round_controller/cl_round_controller.lua")
-include("lobby_manager/cl_lobby.lua")
--- scratstestgame/gamemode/shared.lua
+++
@@ -1,7 +0,0 @@
-GM.Name = "Scrats Zombie SWEP tester"
-
-function GM:Initialize()
-
-    self.BaseClass.Initialize(self)
-
-end
